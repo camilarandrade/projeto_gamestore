b7415aa4914c84d27fb3e1c44b4e56f5dc2fea43
Camila Andrade <camila.rda@outlook.com>
2024-07-10 14:09:31 -0300

    first commit

---
+++ src/main/java/com/gamestore/gamestore/model/Categoria.java
@@ -0,0 +1,60 @@
+package com.gamestore.gamestore.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.gamestore.gamestore.model.Produto;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	 @NotBlank(message = "O nome da categoria é obrigatório!")
+	    @Size(min = 3, max = 100, message = "O nome da categoria deve conter no mínimo 03 e no máximo 100 caracteres")
+	    private String nome;
+	 
+	 @OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+		@JsonIgnoreProperties("categoria")
+		private List<Produto> produtos;
+	 
+	 public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProduto(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
+	
+
+}
